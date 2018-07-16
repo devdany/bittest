087e07dd733c2b0dca0d1acf0b7eb1b9ffac6127
곽대용 <dayong7387@hanmail.net>
2018-07-16 17:35:27 +0900

    de

--- call.js
+++ call.js
@@ -17,6 +17,8 @@ var call = (method, param) => {
             dataString = '{"jsonrpc": "1.0", "id":"curltest", "method": "'+method+'", "params": [] }';
         }
 
+        console.log(dataString);
+
         var options = {
             url: 'http://127.0.0.1:51473/',
             method: 'POST',
