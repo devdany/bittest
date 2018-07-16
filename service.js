ea4e8a71f6879151c31b337360eeb3118ff98432
곽대용 <dayong7387@hanmail.net>
2018-07-16 16:43:46 +0900

    de

--- service.js
+++ service.js
@@ -2,25 +2,30 @@ var call = require('./call');
 
 var service = {
     getLatestBlock: () => {
-        call('getblockcount').then(result => {
-            console.log('-----1-----')
-            console.log(JSON.parse(result).result);
-            return JSON.parse(result).result;
-        }).then(count => {
-            console.log(count);
-            const offset = count -9;
-            const blockInfos = []
-            for(let i = offset; i<=count; i++){
-                call('getblockhash',i).then(result => {
-                    const hash = JSON.parse(result).result;
-                    call('getblock', hash).then(result => {
-                        const blockInfo = JSON.parse(result).result;
-                        blockInfos.push(blockInfo)
-                    })
-                })
-            }
 
-            return blockInfos;
+        return new Promise((resolve, reject) => {
+            call('getblockcount').then(result => {
+                console.log('-----1-----')
+                console.log(JSON.parse(result).result);
+                return JSON.parse(result).result;
+            }).then(count => {
+                console.log(count);
+                const offset = count -9;
+                const blockInfos = []
+                for(let i = offset; i<=count; i++){
+                    call('getblockhash',i).then(result => {
+                        const hash = JSON.parse(result).result;
+                        call('getblock', hash).then(result => {
+                            const blockInfo = JSON.parse(result).result;
+                            blockInfos.push(blockInfo)
+                        })
+                    })
+                }
+
+                return resolve(blockInfos);
+            }).catch(err => {
+                return reject(err);
+            })
         })
     }
 }
