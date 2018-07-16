39d8faaacee239100eadb7bf593e8c619784f56c
곽대용 <dayong7387@hanmail.net>
2018-07-16 17:44:53 +0900

    de

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,9 @@ var router = express.Router();
 var service = require('../service');
 
 router.get('/getLatestBlock', (req, res) => {
-    console.log(service.getLatestBlock());
-    //res.send(service.getLatestBlock());
+    service.getLatestBlock().then(result => {
+        res.send(result);
+    })
 })
 
 
