42c7fc540674c5db3c3e51c02c6ada87357aabb8
곽대용 <dayong7387@hanmail.net>
2018-07-17 12:06:16 +0900

    de

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,16 @@ var express = require('express');
 var router = express.Router();
 var service = require('../service');
 
+
+
 router.get('/getLatestBlock', (req, res) => {
     service.getLatestBlock().then(result => {
+        res.send(result.reverse());
+    })
+})
+
+router.get('/getBlock/:p', (req, res) => {
+    service.getBlock(req.params.p).then(result => {
         res.send(result.reverse());
     })
 })
--- service.js
+++ service.js
@@ -1,5 +1,7 @@
 var call = require('./call');
 
+const idxPerPage = 10;
+
 var service = {
     getLatestBlock: () => {
         return new Promise((resolve, reject) => {
@@ -7,6 +9,44 @@ var service = {
                 return JSON.parse(result).result;
             }).then(async count => {
                 const offset = count -9;
+                const blockInfoIterator = []
+
+                for(let i = offset; i<=count; i++){
+                    blockInfoIterator.push(i)
+                }
+
+                return blockInfoIterator;
+            }).then(async iterator => {
+                return await Promise.all(iterator.map(async val => {
+                    return await call('getblockhash',val).then(async result => {
+                        return await JSON.parse(result).result;
+                    }).catch(err => {
+                        console.log(err);
+                    })
+                }))
+            }).then(async hashes => {
+                return await Promise.all(hashes.map(async hash => {
+                    return await call('getblock', '"'+hash+'"').then(async blockinfo => {
+                        return await JSON.parse(blockinfo).result
+                    }).catch(err => {
+                        console.log(err);
+                    })
+                }))
+            }).then(results => {
+                return resolve(results);
+            }).catch(err => {
+                return reject(err);
+            })
+        })
+    },
+    getBlock: (page) => {
+        return new Promise((resolve, reject) => {
+            call('getblockcount').then(result => {
+                return JSON.parse(result).result
+            }).then(async count => {
+                count = count - (page -1)*idxPerPage;
+
+                const offset = count - (idxPerPage-1);
                 const blockInfoIterator = []
 
                 for(let i = offset; i<=count; i++){
