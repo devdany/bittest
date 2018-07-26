081722cbb67cb364225c9e6c9057f2355a52a6ef
곽대용 <dayong7387@hanmail.net>
2018-07-26 15:25:42 +0900

    de

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var call = require('./call');
+var cors = require('cors')
 
 
 var indexRouter = require('./routes/index');
@@ -13,7 +14,7 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
+app.use(cors())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
